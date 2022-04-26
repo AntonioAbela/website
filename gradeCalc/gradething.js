d10437f9374bdb13ace43dc16604b7ed66ca3abf
AntonioAbela <antonioabela3609@gmail.com>
2022-04-25 20:20:32 -0400

    added class title to output

--- gradeCalc/gradething.js
+++ gradeCalc/gradething.js
@@ -1,16 +1,30 @@
 const StudentVue = require('studentvue.js');
+var async = require('async');
+
+putObject();
 dothing();
-let grades;
+//let grades;
 let mpObject = [];
 let allGrades = [];
 //let domObject = document.getElementById('domObject');
 
-for (let i = 1; i <= 5; i += 2) {
-    mpObject.push(getGrades('https://va-arl-psv.edupoint.com/', '986134', 'xbnu4gnl', i))
-    grades = getGrades('https://va-arl-psv.edupoint.com/', '986134', 'xbnu4gnl', i);
-    
-    
+async function putObject() {
+    for (let i = 1; i <= 5; i += 2) {
+        let testthing = await (getGrades('https://va-arl-psv.edupoint.com/', '986134', 'xbnu4gnl', i))
+        mpObject.push(testthing)
+        testthing = ''
+        console.log(i)
+        console.log(mpObject)
+        if (i === 5) {
+            callGrades();
+        } 
+    }
 }
+
+
+/*for (let i = 1; i <= 5; i += 2) {
+    mpObject.push(getGrades('https://va-arl-psv.edupoint.com/', '986134', 'xbnu4gnl', i)) 
+}*/
 
 
 function getGrades(url, username, password, reportingPeriod)  {
@@ -36,12 +50,12 @@ function callGrades() {
    // console.log(grades['_rejectionHandler0']['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['CalculatedScoreRaw'] + '%');
     for (let j = 0; j <= 2; j++) {
         if (j === 1) {
-            allGrades.push(mpObject[j]['_rejectionHandler0']['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['0']['CalculatedScoreRaw'])
-            console.log(mpObject[j]['_rejectionHandler0']['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['0']['CalculatedScoreRaw'])
+            allGrades.push(mpObject[j]['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['0']['CalculatedScoreRaw'])
+            console.log(mpObject[j]['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['0']['CalculatedScoreRaw'])
         }
         else {
-            allGrades.push(mpObject[j]['_rejectionHandler0']['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['CalculatedScoreRaw'])
-            console.log(mpObject[j]['_rejectionHandler0']['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['CalculatedScoreRaw'])
+            allGrades.push(mpObject[j]['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['CalculatedScoreRaw'])
+            console.log(mpObject[j]['Gradebook']['Courses']['Course']['0']['Marks']['Mark']['CalculatedScoreRaw'])
         } 
     } 
     console.log(allGrades)
@@ -50,17 +64,17 @@ function callGrades() {
 
 function computeLowestGrade(allGrades) {
     let totalNeeded = 358;
-    let total = 0;
+    let totalCurrent = 0;
     for (let i = 0; i < allGrades.length; i++) {
-        total += parseInt(allGrades[i])
+        totalCurrent += parseInt(allGrades[i])
         
     }
-    console.log(totalNeeded - total)
+    console.log(totalNeeded - totalCurrent)
     //domObject.innerHTML(totalNeeded - total);
 
 }
 
-setTimeout( function () { callGrades(); }, 60000)
+//setTimeout( function () { callGrades(); }, 50000)
 
 function dothing() {
     console.log('');
