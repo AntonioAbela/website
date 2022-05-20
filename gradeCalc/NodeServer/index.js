c9d2f9eb12179f86ffaffb1fbd5567984f144f0d
AntonioAbela <antonioabela3609@gmail.com>
2022-05-20 15:01:37 -0400

    updated studentvue project

--- gradeCalc/NodeServer/index.js
+++ gradeCalc/NodeServer/index.js
@@ -13,9 +13,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
@@ -23,7 +20,7 @@ server.listen(3000, () => {
 
 io.on('connection', (socket) => {
     socket.on('getGrades', (username, password) => {
-    //console.log(username + ' ' + password)
+        console.log('a user connected')
         putObject(username, password)
     });
   
@@ -32,15 +29,10 @@ io.on('connection', (socket) => {
 let mpObject = [];
 let allGrades = [];
 let className = '';
-let grades = {
-    a: 89.5,
-    b: 79.5,
-    c: 69.5,
-    d: 59.5
-}
 
 
 async function putObject(username, password) {
+    console.log('Request recieved')
     mpObject = [];
     for (let i = 1; i <= 5; i += 2) {
         let testthing = await (getGrades('https://va-arl-psv.edupoint.com/', username, password, i))
@@ -71,56 +63,32 @@ function getGrades(url, username, password, reportingPeriod)  {
 function callGrades() { 
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j <= 2; j++) {
-            if (j === 1) {
-                allGrades.push(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['0']['CalculatedScoreRaw'])
-                //console.log(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['0']['CalculatedScoreRaw'])
-            }
-            else {
-                allGrades.push(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['CalculatedScoreRaw'])
-                //console.log(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['CalculatedScoreRaw'])
-            }
-            className = mpObject[j]['Gradebook']['Courses']['Course'][i];
             
+            try {
+                
+                if (j === 1) {
+                    allGrades.push(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['0']['CalculatedScoreRaw'])
+                    console.log(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['0']['CalculatedScoreRaw'])
+                }
+                else {
+                    allGrades.push(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['CalculatedScoreRaw'])
+                    console.log(mpObject[j]['Gradebook']['Courses']['Course'][i]['Marks']['Mark']['CalculatedScoreRaw'])
+                
+                }
+                className = mpObject[j]['Gradebook']['Courses']['Course'][i];    
+            }
+            catch(err) {
+                break
+            }
     }
     let classNamed = className['Title']
     socket.emit('sendClient', allGrades, classNamed)
-    //console.log(allGrades, classNamed)
-    //console.log(className)
-    //computeLowestGrade(allGrades);
     allGrades = [];
     className = '';
     classNamed = '';
 } 
 }
 
-
-
-function computeLowestGrade(allGrades, grade,) {
-    let zero = 0;
-    for (let array = 0; array < allGrades.length; array++) {
-        if(allGrades[array] == 0) {zero++}
-    }
-    let gradeWanted = (allGrades.length - zero) * grades[grade] + grades[grade];
-    let totalCurrent = 0;
-    for (let i = 0; i < allGrades.length; i++) {
-            totalCurrent += parseInt(allGrades[i])
-    }
-    console.log(roomId)
-    if (grade === 'a') {
-        console.log('You need a ' + (gradeWanted - totalCurrent) + '% to get an ' + grade.toUpperCase() + ' in ' + className['Title'])
-        finalMessage = 'You need a ' + (gradeWanted - totalCurrent) + '% to get an ' + grade.toUpperCase() + ' in ' + className['Title']
-        
-        /*io.emit("needed", 'You need a ' + (gradeWanted - totalCurrent) + '% to get an ' + grade.toUpperCase() + ' in ' + className['Title'], roomId)*/
-
-
-    }
-    else {
-        console.log('You need a ' + (gradeWanted - totalCurrent) + '% to get a ' + grade.toUpperCase() + ' in ' + className['Title'])
-        finalMessage = 'You need a ' + (gradeWanted - totalCurrent) + '% to get a ' + grade.toUpperCase() + ' in ' + className['Title']
-       
-        /*io.emit("needed", 'You need a ' + (gradeWanted - totalCurrent) + '% to get a ' + grade.toUpperCase() + ' in ' + className['Title'], roomId) */
-    }
-}
 
 });
 
